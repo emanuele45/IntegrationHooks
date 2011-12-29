c1b6b4efa7e96eff600b4a21267203f98434c957
emanuele <emanuele45@interfree.it>
2011-12-29 16:55:07 +0100

    initial commit

---
+++ IntegrationHooks.english.php
@@ -0,0 +1,12 @@
+<?php
+
+$txt['hooks_title_list'] = 'Integration Hooks';
+$txt['hooks_field_hook_name'] = 'Hook Name';
+$txt['hooks_field_function_name'] = 'Function Name';
+$txt['hooks_field_hook_exists'] = 'Exists';
+$txt['hooks_exists'] = 'Exists';
+$txt['hooks_missing'] = 'Missing';
+$txt['hooks_no_hooks'] = 'There are not any hooks setup.';
+$txt['hooks_button_remove'] = 'Remove Selected';
+
+?>
---
+++ install.php
@@ -0,0 +1,20 @@
+<?php
+
+if (file_exists(dirname(__FILE__) . '/SSI.php') && !defined('SMF'))
+    require_once(dirname(__FILE__) . '/SSI.php');
+elseif (!defined('SMF'))
+    exit('<b>Error:</b> Cannot install - please verify you put this in the same place as SMF\'s index.php.');
+
+$hooks = array(
+    'integrate_admin_include' => '$sourcedir/Subs-IntegrationHooks.php',
+    'integrate_admin_areas' => 'hooks_admin_areas',
+    'integrate_modify_modifications' => 'hooks_modify_modifications',
+);
+
+foreach ($hooks as $hook => $function)
+    add_integration_function($hook, $function);
+
+if (SMF == 'SSI')
+    echo 'Database adaptation successful!';
+
+?>
